8efdca08544da27b9ddb8acfd9dc658e2280d4e8
Emil Møller <emilrmoeller@gmail.com>
2014-08-27 15:48:21 +0200

    Initial

---
+++ app/index.js
@@ -0,0 +1,221 @@
+'use strict';
+var fs = require('fs');
+var util = require('util');
+var path = require('path');
+var spawn = require('child_process').spawn;
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var wiredep = require('wiredep');
+
+var AppGenerator = module.exports = function Appgenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  // setup the test-framework property, gulpfile template will need this
+  this.testFramework = options['test-framework'] || 'mocha';
+
+  // for hooks to resolve on mocha by default
+  options['test-framework'] = this.testFramework;
+
+  // resolved to mocha by default (could be switched to jasmine for instance)
+  this.hookFor('test-framework', {
+    as: 'app',
+    options: {
+      options: {
+        'skip-install': options['skip-install-message'],
+        'skip-message': options['skip-install']
+      }
+    }
+  });
+
+  this.options = options;
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(AppGenerator, yeoman.generators.Base);
+
+AppGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // welcome message
+  if (!this.options['skip-welcome-message']) {
+    console.log(this.yeoman);
+    console.log(chalk.magenta('Out of the box I include HTML5 Boilerplate, jQuery, and a gulpfile.js to build your app.'));
+  }
+
+  var prompts = [{
+    type: 'checkbox',
+    name: 'features',
+    message: 'What more would you like?',
+    choices: [{
+      name: 'Sass',
+      value: 'includeSass',
+      checked: true
+    }, {
+      name: 'Foundation (Needs Sass to be installed)',
+      value: 'includeFoundation',
+      checked: true
+    }, {
+      name: 'Modernizr',
+      value: 'includeModernizr',
+      checked: true
+    }, {
+      name: 'Assemble',
+      value: 'includeAssemble',
+      checked: false
+    }]
+  }];
+
+  this.prompt(prompts, function (answers) {
+    var features = answers.features;
+
+    var hasFeature = function (feat) {
+      return features.indexOf(feat) !== -1;
+    }
+
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    this.includeSass = hasFeature('includeSass');
+    this.includeFoundation = hasFeature('includeFoundation');
+    this.includeModernizr = hasFeature('includeModernizr');
+    this.includeAssemble = hasFeature('includeAssemble');
+
+    cb();
+  }.bind(this));
+};
+
+AppGenerator.prototype.gulpfile = function () {
+  this.template('gulpfile.js');
+};
+
+AppGenerator.prototype.packageJSON = function () {
+  this.template('_package.json', 'package.json');
+};
+
+AppGenerator.prototype.git = function () {
+  this.copy('gitignore', '.gitignore');
+  this.copy('gitattributes', '.gitattributes');
+};
+
+AppGenerator.prototype.bower = function () {
+  this.copy('bowerrc', '.bowerrc');
+  this.copy('bower.json', 'bower.json');
+};
+
+AppGenerator.prototype.jshint = function () {
+  this.copy('jshintrc', '.jshintrc');
+};
+
+AppGenerator.prototype.editorConfig = function () {
+  this.copy('editorconfig', '.editorconfig');
+};
+
+AppGenerator.prototype.h5bp = function () {
+  this.copy('favicon.ico', 'app/favicon.ico');
+  this.copy('404.html', 'app/404.html');
+  this.copy('robots.txt', 'app/robots.txt');
+  this.copy('htaccess', 'app/.htaccess');
+};
+
+AppGenerator.prototype.assemble = function () {
+  if (this.includeAssemble) {
+    this.copy('default.hbs', 'app/templates/layouts/default.hbs');
+  }
+};
+
+
+AppGenerator.prototype.mainStylesheet = function () {
+  var css = 'main.' + (this.includeSass ? 's' : '') + 'css';
+  this.copy(css, 'app/styles/' + css);
+  this.copy('_variables.scss', 'app/styles/_variables.scss');
+
+  if (this.includeFoundation) {
+    this.copy('_foundation-settings.scss', 'app/styles/_foundation-settings.scss');
+  }
+};
+
+AppGenerator.prototype.writeIndex = function () {
+  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+  this.indexFile = this.engine(this.indexFile, this);
+
+  // wire Foundation plugins
+  if (this.includeFoundation) {
+    var bs = 'bower_components/foundation/js/foundation.js';
+    this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
+      bs
+    ]);
+  }
+
+  this.indexFile = this.appendFiles({
+    html: this.indexFile,
+    fileType: 'js',
+    optimizedPath: 'scripts/main.js',
+    sourceFileList: ['scripts/main.js']
+  });
+};
+
+AppGenerator.prototype.app = function () {
+  this.mkdir('app');
+  this.mkdir('app/scripts');
+  this.mkdir('app/styles');
+
+  if (this.includeSass) {
+    this.mkdir('app/styles/base');
+    this.mkdir('app/styles/elements');
+    this.mkdir('app/styles/modules');
+    this.mkdir('app/styles/plugins');
+  }
+
+  this.mkdir('app/images');
+
+  if (this.includeAssemble) {
+    this.mkdir('app/templates');
+    this.mkdir('app/templates/layouts');
+    this.mkdir('app/templates/pages');
+    this.mkdir('app/templates/partials');
+  }
+
+  this.write('app/index.html', this.indexFile);
+  this.write('app/scripts/main.js', 'console.log(\'\\\'Allo \\\'Allo!\');');
+};
+
+
+AppGenerator.prototype.install = function () {
+  var howToInstall =
+    '\nAfter running `npm install & bower install`, inject your front end dependencies into' +
+    '\nyour HTML by running:' +
+    '\n' +
+    chalk.yellow.bold('\n  gulp wiredep');
+
+  if (this.options['skip-install']) {
+    console.log(howToInstall);
+    return;
+  }
+
+  var done = this.async();
+  this.installDependencies({
+    skipMessage: this.options['skip-install-message'],
+    skipInstall: this.options['skip-install'],
+    callback: function () {
+      var bowerJson = JSON.parse(fs.readFileSync('./bower.json'));
+
+      // wire Bower packages to .html
+      wiredep({
+        bowerJson: bowerJson,
+        directory: 'app/bower_components',
+        exclude: ['foundation'],
+        src: 'app/index.html'
+      });
+
+      if (this.includeSass) {
+        // wire Bower packages to .scss
+        wiredep({
+          bowerJson: bowerJson,
+          directory: 'app/bower_components',
+          src: 'app/styles/*.scss'
+        });
+      }
+
+      done();
+    }.bind(this)
+  });
+};
---
+++ app/templates/gulpfile.js
@@ -0,0 +1,149 @@
+'use strict';
+// generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+
+var gulp = require('gulp');
+<% if (includeAssemble) { %>
+    var assemble    = require('gulp-assemble');
+<% } %>
+
+// load plugins
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+
+gulp.task('styles', function () {<% if (includeSass) { %>
+    return gulp.src('app/styles/main.scss')
+        .pipe($.plumber())
+        .pipe($.rubySass({
+            style: 'expanded',
+            precision: 10
+        }))<% } else { %>
+    return gulp.src('app/styles/main.css')<% } %>
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('.tmp/styles'))
+        .pipe($.size())
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter(require('jshint-stylish')))
+        .pipe($.size());
+});
+
+<% if (includeAssemble) { %>
+    var options = {
+      data: 'app/data/*.json',
+      partials: 'app/templates/partials/*.hbs',
+      layoutdir: 'app/templates/layouts/',
+      layout: 'default.hbs'
+    };
+
+    gulp.task('assemble', function () {
+      gulp.src(['app/templates/*.hbs', 'app/templates/pages/*.hbs'])
+        .pipe(assemble(options))
+        .pipe(gulp.dest('app'));
+    });
+<% } %>
+
+gulp.task('html', ['styles', 'scripts'], function () {
+    var jsFilter = $.filter('**/*.js');
+    var cssFilter = $.filter('**/*.css');
+
+    return gulp.src('app/*.html')
+        .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+        .pipe(jsFilter)
+        .pipe($.uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe($.csso())
+        .pipe(cssFilter.restore())
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size());
+});
+
+gulp.task('fonts', function () {
+    return $.bowerFiles()
+        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe($.size());
+});
+
+gulp.task('extras', function () {
+    return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
+});
+
+gulp.task('build', [<% if (includeAssemble) { %>'assemble'<% } %>, 'html', 'images', 'fonts', 'extras']);
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+gulp.task('serve', ['browser-sync'<% if (includeSass) { %>, 'styles'<% } %>, <% if (includeAssemble) { %>'assemble'<% } %>], function () {});
+
+// browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: ["app", ".tmp"]
+        }
+    });
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+    var wiredep = require('wiredep').stream;
+<% if (includeSass) { %>
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep({
+            directory: 'app/bower_components'
+        }))
+        .pipe(gulp.dest('app/styles'));
+<% } %>
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_components'<% if (includeSass && includeFoundation) { %>,
+            exclude: ['foundation']<% } %>
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('watch', ['browser-sync', 'serve'], function () {
+
+    // watch for changes
+
+    gulp.watch([
+        'app/*.html',
+        '.tmp/styles/**/*.css',
+        'app/scripts/**/*.js',
+        'app/images/**/*'
+    ]).on('change', function (file) {
+        browserSync.reload();
+    });
+    <% if (includeAssemble) { %>
+        gulp.watch('app/templates/**/*.hbs', ['assemble']);
+    <% } %>
+    gulp.watch('app/styles/**/*.<%= includeSass ? 'scss' : 'css' %>', ['styles']);
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('bower.json', ['wiredep']);
+});
